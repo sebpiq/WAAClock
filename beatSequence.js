36b76ccfbf5de412732d40c0dfbc4dc87378f66f
sebpiq <sebpiq@gmail.com>
2013-07-06 14:35:05 +0400

    Remove verbose comments

--- beatSequence.js
+++ beatSequence.js
@@ -5,24 +5,12 @@ var context = typeof AudioContext === 'undefined' ? new webkitAudioContext() : n
   , beatDur = 60/tempo, barDur = signature * beatDur
   , clock = new WAAClock(context, {tickTime: 0.05, lookAheadTime: 0.1})
 
-// The following code highlights the current beat in the UI by calling the function `uiNextBeat` periodically.  //
-// Using `WAAClock.setTimeout` we create an event that will be executed at t=0, then repeated every `beatDur`   //
-// (so at t=0, t=1*beatDur, t=2*beatDur, ...).                                                                  //
-// We also set a very big tolerance, because we don't want an event to be dropped just because it is late.      //
+// The following code highlights the current beat in the UI by calling the function `uiNextBeat` periodically.
 var event = clock.callbackAtTime(uiNextBeat, 0)
   .repeat(beatDur)
   .tolerance(100)
 
-// This function activates the beat `beatInd` of `track`.                                                       //
-// First we calculate the time of the next `beatInd`, and we schedule the node to start playing then, using     //
-// `AudioNode.start2`.                                                                                          //
-// Because `AudioNode.start` can be called only once, we cannot use `repeat`, and we have to redo the whole     //
-// thing : create a new node, schedule it with `start2`, loop, ..., once the event has been dealt with.         //
-// This is achieved by adding listeners to 'executed' and 'expired' on the event, and rescheduling things for   // 
-// the next bar.                                                                                                //
-// For the `start` event, we need a small tolerance (10 ms here), because if the beat is activated right when   //
-// it should be played, we will simply hear the beat late. So instead the beat is dropped and rescheduled for   //
-// next bar, this keeping the pulse intact.                                                                     //
+// This function activates the beat `beatInd` of `track`.
 var startBeat = function(track, beatInd) {
   var scheduleBeat = function(time) {
     var bufferNode = soundBank[track]()
@@ -35,10 +23,10 @@ var startBeat = function(track, beatInd) {
   scheduleBeat(nextBeatTime(beatInd))
 }
 
-// This function deactivates the beat `beatInd` of `track`
+// This function deactivates the beat `beatInd` of `track`.
 var stopBeat = function(track, beatInd) {
   var event = beats[track][beatInd]
-  event.clear()                           // If the beat is deactivated, we simply clear the upcoming event.
+  event.clear()
   event.removeAllListeners()
 }
 
